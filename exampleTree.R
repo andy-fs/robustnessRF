705dc898862a590d9d058cf2c9f936ccffac1e8f
andy-fs <a.slettenhaar@outlook.com>
2024-12-18 15:53:17 +0100

    Update exampleTree.R

--- exampleTree.R
+++ exampleTree.R
@@ -1,17 +1,27 @@
 library(data.tree)
-library(DiagrammeR)
-library(networkD3)
-
 
 startNode <- Node$new("X1=3.5")
 left <- startNode$AddChild("X1=1.5")
 leftLeft <- left$AddChild("(1,2,4)")
+leftRight <- left$AddChild("X2=5")
+leftRightLeft <- leftRight$AddChild("(3,4,6)")
+leftRightRight <- leftRight$AddChild("(2,6,5)")
+right <- startNode$AddChild("X2=9")
+rightLeft <- right$AddChild("(4,8,10)")
+rightRight <- right$AddChild("(5,10,14)")
+
+myTree <- ToDataFrameNetwork(startNode, "ExampleTree")
+plot(startNode)
+
+startNode2 <- Node$new("X1=2.5")
+left <- startNode2$AddChild("X2=4")
+leftLeft <- left$AddChild("(1,2,4)")
 leftRight <- left$AddChild("(2,6,5)")
-right <- startNode$AddChild("X2=6")
-rightLeft <- right$AddChild("(3,4,6)")
+right <- startNode2$AddChild("X2=6")
+rightLeft <- right$AddChild("(3,4,16)")
 rightRight <- right$AddChild("X1=4.5")
 rightRightLeft <- rightRight$AddChild("(4,8,10)")
 rightRightRight <- rightRight$AddChild("(5,10,14)")
 
 myTree <- ToDataFrameNetwork(startNode, "ExampleTree")
-plot(startNode)
+plot(startNode2)
